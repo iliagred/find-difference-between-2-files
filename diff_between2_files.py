b47ae26ccf97fc90b084303d9ee2da8154a7e8bd
iliagred <33458759+iliagred@users.noreply.github.com>
2018-12-05 13:14:03 +0200

    find difference between 2 files

---
+++ diff_between2_files.py
@@ -0,0 +1,23 @@
+import os
+#change directors where locate files
+#os.chdir('D:/python')
+os.chdir('D:/')
+print(os.getcwd())
+print('Please put location file1, Exemple:D:/python/file1 ')
+file1 = input()
+print('Please put location file2, Exemple:D:/python/file2 ')
+file2 = input()
+print('Please check if you file location corect '+file1)
+print('Please check if you file location corect '+file2)
+
+with open(file1,'r') as file1:
+	with open(file2,'r') as file2:
+		same=set(file1).symmetric_difference(file2)
+
+same.discard('\n')
+with open('output','w') as file_out:
+	for line in same:
+		file_out.write(line)
+showmyresult = open('output', 'r')
+
+print(showmyresult.read())		
